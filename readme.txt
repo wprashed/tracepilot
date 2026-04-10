3f32484a5390f892ab47f4037a79a6dbd36012f4
Md Rashed Hossain <wprashed@icloud.com>
2026-04-10 13:09:02 +0600

    version update

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: activity log, audit log, security, diagnostics, monitoring, logging
 Requires at least: 6.0
 Tested up to: 6.9.4
 Requires PHP: 7.4
-Stable tag: 1.3.1
+Stable tag: 1.0.0
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
--- tracepilot-for-wordpress.php
+++ tracepilot-for-wordpress.php
@@ -3,7 +3,7 @@
  * Plugin Name: TracePilot for WordPress
  * Plugin URI: https://rashed.im/
  * Description: Activity logging, diagnostics, threat review, and export tooling for WordPress administrators.
- * Version: 1.3.2
+ * Version: 1.0.0
  * Author: Rashed Hossain
  * Author URI: https://rashed.im/
  * Text Domain: wp-activity-logger-pro
